2de4589a19c258ed7eaf6e9427c0de1051266b12
John C. Burnham <jcb@johnchandlerburnham.com>
2020-03-26 22:57:18 -0400

    Haskell HOAS conversion functions, reduction and normalization

--- FormalityCore.hs
+++ FormalityCore.hs
@@ -12,14 +12,15 @@ import Prelude hiding (all, mod)
 
 type Name = String
 type Done = Bool
+type Eras = Bool
 
 data Term
   = Var Int                  -- Variable
   | Ref Name                 -- Reference
   | Typ                      -- Type type
-  | All Bool Name Term Term  -- Forall
-  | Lam Bool Name Term       -- Lambda
-  | App Bool Term Term       -- Application
+  | All Eras Name Term Term  -- Forall
+  | Lam Eras Name Term       -- Lambda
+  | App Eras Term Term       -- Application
   | Slf Name Term            -- Self-type
   | Ins Term Term            -- Self-type introduction
   | Eli Term                 -- Self-type elimination
@@ -194,3 +195,154 @@ stringifyDef (Def n t d) =
 
 stringifyMod :: Module -> String
 stringifyMod = foldl (\s d -> s ++ "\n\n" ++ stringifyDef d) ""
+
+-- Substitution
+
+shift :: Int -> Int -> Term -> Term
+shift inc dep term = let go x = shift inc dep x in case term of
+  Var i        -> Var (if i < dep then i else (i + inc))
+  Ref n        -> Ref n
+  Typ          -> Typ
+  All e n h b  -> All e n (go h) (shift inc (dep + 1) b)
+  Lam e n b    -> Lam e n (shift inc (dep + 1) b)
+  App e f a    -> App e (go f) (go a)
+  Slf n t      -> Slf n (shift inc (dep + 1) t)
+  Ins t x      -> Ins (go t) (go x)
+  Eli x        -> Eli (go x)
+  Ann d t x    -> Ann d (go t) (go x)
+
+-- substitute a value for an index at a certain depth in a term
+subst :: Term -> Int -> Term -> Term
+subst v dep term =
+  let v'   = shift 1 0 v
+      go x = subst v dep x
+  in case term of
+  Var i       -> if i == dep then v else Var (i - if i > dep then 1 else 0)
+  Ref n       -> Ref n
+  Typ         -> Typ
+  All e n h b -> All e n (go h) (subst v' (dep + 1) b)
+  Lam e n b   -> Lam e n (subst v' (dep + 1) b)
+  App e f a   -> App e (go f) (go a)
+  Slf n t     -> Slf n (subst v' (dep + 1) t)
+  Ins t x     -> Ins (go t) (go x)
+  Eli x       -> Eli (go x)
+  Ann d t x   -> Ann d (go t) (go x)
+
+-- Erase computationally irrelevant terms
+erase :: Term -> Term
+erase term = let go = erase in case term of
+  All e n h b  -> All e n (go h) (go b)
+  Lam True n b -> (subst (Ref "<erased>") 0 b) 
+  Lam e    n b -> Lam e n (go b)
+  App True f a -> go f
+  App e    f a -> App e (go f) (go a)
+  Slf n t      -> Slf n (go t)
+  Ins t x      -> go x
+  Eli x        -> go x
+  Ann d t x    -> go x
+  _            -> term
+
+-- lookup the value of an expression in a module
+deref :: Name -> Module -> Term
+deref n defs = maybe (Ref n) _term (M.lookup n defs)
+
+-- lower-order interpreter
+evalTerm :: Term -> Module -> Term
+evalTerm term mod = go term
+  where
+  go :: Term -> Term
+  go t = case t of
+    All e n h b -> All e n h b
+    Lam e n b   -> Lam e n (go b)
+    App e f a -> case go f of
+      Lam e n b -> go (subst a 0 b)
+      f         -> App e f (go a)
+    Ins t x   -> go x
+    Eli x     -> go x
+    Ann d t x -> go x
+    Ref n     -> case (deref n mod) of
+      Ref m   -> if n == m then Ref m else go (deref n mod)
+      x       -> go x
+    _         -> term
+
+data TermH
+  = VarH Int
+  | RefH Name
+  | TypH
+  | AllH Eras Name TermH (TermH -> TermH)
+  | LamH Eras Name (TermH -> TermH)
+  | AppH Eras TermH TermH
+  | SlfH Name (TermH -> TermH)
+  | InsH TermH TermH
+  | EliH TermH
+  | AnnH Bool TermH TermH
+
+toTermH :: Term -> TermH
+toTermH t = go [] t
+  where
+    go :: [TermH] -> Term -> TermH
+    go vs t = case t of
+      Var i       -> if i < length vs then vs !! i else VarH i 
+      Ref n       -> RefH n
+      Typ         -> TypH
+      All e n h b -> AllH e n (go vs h) (\x -> go (x : vs) b)
+      Lam e n b   -> LamH e n (\x -> go (x : vs) b)
+      App e f a   -> AppH e (go vs f) (go vs a)
+      Slf n t     -> SlfH n (\x -> go (x : vs) t)
+      Ins t x     -> InsH (go vs t) (go vs x)
+      Eli x       -> EliH (go vs x)
+      Ann d t x   -> AnnH d (go vs t) (go vs x)
+
+fromTermH :: TermH -> Term
+fromTermH t = go 0 t
+  where
+    go :: Int -> TermH -> Term
+    go dep t = case t of
+      VarH n       -> Var (dep - n)
+      RefH n       -> Ref n
+      TypH         -> Typ
+      AllH e n h b -> All e n (go dep h) (go (dep + 1) (b $ VarH dep))
+      LamH e n b   -> Lam e n (go (dep + 1) (b $ VarH dep))
+      AppH e f a   -> App e (go dep f) (go dep a)
+      SlfH n t     -> Slf n (go (dep + 1) (t $ VarH dep))
+      InsH t x     -> Ins (go dep t) (go dep x)
+      EliH x       -> Eli (go dep x)
+      AnnH d t x   -> Ann d (go dep t) (go dep x)
+
+reduceTermH :: Module -> TermH -> TermH
+reduceTermH defs t = go t
+  where
+    go :: TermH -> TermH
+    go t = case t of
+      RefH n       -> case deref n defs of
+        Ref m   -> RefH m
+        x       -> go (toTermH x)
+      LamH True n b   -> (b $ RefH "<erased>") 
+      AppH True f a -> go f
+      AppH False f a -> case go f of
+        LamH e n b -> go (b a)
+        f          -> AppH False f (go a)
+      InsH t x   -> go x
+      EliH x     -> go x
+      AnnH d t x -> go x
+      _          -> t
+
+normalizeTermH :: Module -> TermH -> TermH
+normalizeTermH defs t = go t
+  where
+    go :: TermH -> TermH
+    go t = case t of
+      AllH e n h b -> AllH e n (go h) (\x -> go $ b x)
+      LamH e n b   -> LamH e n (\x -> go $ b x)
+      AppH e f a   -> AppH e (go f) (go a)
+      SlfH n t     -> SlfH n (\x -> go $ t x)
+      InsH t x   -> go x
+      EliH x     -> go x
+      AnnH d t x -> go x
+      _          -> t
+
+reduce :: Module -> Term -> Term
+reduce defs = fromTermH . reduceTermH defs . toTermH
+
+normalize :: Module -> Term -> Term
+normalize defs = fromTermH . normalizeTermH defs . toTermH
